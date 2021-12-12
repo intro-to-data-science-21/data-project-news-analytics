7172cecdaa4ce012c43de3538192329e2b6792a6
koenoosthoek <koenoosthoek@gmail.com>
2021-12-12 16:50:29 +0100

    git config pull.rebase false

--- 02-process-data.R
+++ 02-process-data.R
@@ -1,6 +1,7 @@
 # 02-process-data ---------------------------------------------------------
 
-headlines <- qpcR:::cbind.na(guardian, nytimes, times, wsj, independent)
+headlines <- qpcR:::cbind.na(guardian, times, dailymail, independent, mirror, telegraph,
+                             nytimes, wsj, usatoday, washingtonpost, latimes, tampabay, startribune)
 
 headlines <- as.data.frame(headlines)
 
